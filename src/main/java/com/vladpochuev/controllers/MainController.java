bb007d315aeae5b9fe410bddfe6c69b883eac9f2
vladpochuev <pochuev.vladislav@gmail.com>
2023-09-27 20:26:41 +0300

    Add opportunity to make links temporary

--- src/main/java/com/vladpochuev/controllers/MainController.java
+++ src/main/java/com/vladpochuev/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.vladpochuev.controllers;
 import com.vladpochuev.dao.BinDAO;
 import com.vladpochuev.model.Bin;
 import com.vladpochuev.service.HashGenerator;
+import com.vladpochuev.service.LinkHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,10 +24,13 @@ public class MainController {
     }
 
     @PostMapping()
-    public String submitBin(@ModelAttribute("bin") Bin bin) {
+    public String submitBin(@ModelAttribute("bin") Bin bin,
+                            @ModelAttribute("amountOfTime") String amountOfTime) {
         HashGenerator hashGenerator = new HashGenerator(bin.getTitle(), bin.getText());
         bin.setId(hashGenerator.getHash("SHA-1", 10));
         binDAO.create(bin);
+        LinkHandler handler = new LinkHandler(bin.getId(), amountOfTime, binDAO);
+        handler.start();
         return "redirect:/" + bin.getId();
     }
 
---
+++ src/main/java/com/vladpochuev/service/LinkHandler.java
@@ -0,0 +1,51 @@
+package com.vladpochuev.service;
+
+import com.vladpochuev.dao.BinDAO;
+
+
+public class LinkHandler extends Thread {
+    private final String binId;
+    private final String amountOfTime;
+    private final BinDAO binDAO;
+
+    public LinkHandler(String binId, String amountOfTime, BinDAO binDAO) {
+        this.binId = binId;
+        this.binDAO = binDAO;
+        this.amountOfTime = amountOfTime;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Long amountOfTime = getAmountOfTime(this.amountOfTime);
+            if(amountOfTime == null) return;
+            System.out.println("bin " + binId + " was added to queue for " + amountOfTime);
+            Thread.sleep(amountOfTime);
+            System.out.println("bin " + binId + " was removed");
+            binDAO.delete(binId);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Long getAmountOfTime(String time) {
+        AmountOfTime amountOfTime = AmountOfTime.valueOf(time);
+        return amountOfTime.time;
+    }
+
+    enum AmountOfTime {
+        INFINITE(null),
+        ONE_MINUTE(1000 * 60L),
+        TEN_MINUTES(ONE_MINUTE.time * 10),
+        ONE_HOUR(ONE_MINUTE.time * 60),
+        ONE_DAY(ONE_HOUR.time * 24),
+        ONE_WEEK(ONE_DAY.time * 7),
+        ONE_MONTH(ONE_DAY.time * 30),
+        SIX_MONTH(ONE_MONTH.time * 6);
+
+        private final Long time;
+        AmountOfTime(Long time) {
+            this.time = time;
+        }
+    }
+}
